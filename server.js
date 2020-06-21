1a5994c6502be3ac20617f8289c9bb45e63b4504
roxyvaught <roxanna.vaught@gmail.com>
2020-06-21 18:48:17 -0500

    added routing

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Set up Express session and connect to sequelize database
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'alphaDog',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Set up template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
